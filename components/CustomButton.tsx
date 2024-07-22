75c00cad80a1e26a995faa2d47e1eb66be097abf
Gazel <gazel@ngoprekweb.com>
2024-07-22 11:20:59 +0700

    OnBoarding Screen

---
+++ components/CustomButton.tsx
@@ -0,0 +1,20 @@
+import { TouchableOpacity, Text } from "react-native";
+import React from "react";
+
+type CustomButton = {
+  title: string;
+  handlePress: () => void;
+  containerStyles: string;
+  textStyles?: string;
+  isLoading?: boolean;
+};
+
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }: CustomButton) => {
+  return (
+    <TouchableOpacity className={`bg-secondary-100 rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? "opacity-50" : ""}`} onPress={handlePress} activeOpacity={0.7} disabled={isLoading}>
+      <Text className={`text-primary font-psemibold  text-lg ${textStyles}`}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
