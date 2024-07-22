ec0751989544af8c227c0af80e97aa6087fb601c
Gazel <gazel@ngoprekweb.com>
2024-07-22 10:10:28 +0700

    Bottom tabs

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <View>
+      <Text className="text-3xl">Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({});
--- app/index.tsx
+++ app/index.tsx
@@ -4,17 +4,15 @@ import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
 
 const index = () => {
-	return (
-		<View className="bg-blue-50 flex-1 flex items-center justify-center">
-			<Text className="text-3xl font-pblack">Aora!</Text>
-			<StatusBar style="auto" />
-			<Link href={"/profile"} style={{ color: "blue" }}>
-				Go to kocak wak
-			</Link>
-		</View>
-	);
+  return (
+    <View className="bg-blue-50 flex-1 flex items-center justify-center">
+      <Text className="text-5xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href={"/home"} style={{ color: "blue" }}>
+        Go to kocak wak
+      </Link>
+    </View>
+  );
 };
 
 export default index;
-
-
